29190b10299d4df264380e0cb411fc593d4ec2be
Marcelo Soares <nimbfire@gmail.com>
2020-02-12 00:19:22 -0300

    Update stuff

--- game.c
+++ game.c
@@ -1,8 +1,27 @@
 #include <gb/gb.h>
 #include <stdio.h>
 
+#include "sprites/Characters.c"
+
+UBYTE running;
+void init();
 
 void main(void)
 {
-  printf("Game first test\n");
+  init();
+  // printf("Game first test\n");
+  while(running) {
+
+  }
+}
+
+
+void init() {
+  running = 1;
+  DISPLAY_ON;
+  SHOW_SPRITES;
+  set_sprite_data(0,3,Characters);
+  set_sprite_tile(0,2);
+  move_sprite(0, 40, 40);
+
 }
